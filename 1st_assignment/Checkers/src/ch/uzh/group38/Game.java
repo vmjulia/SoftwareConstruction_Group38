b0ce07f1bef6cc8e95cce7519a4c9e834ff431f8
grueezi <mark.rueetschi@uzh.ch>
2021-10-15 11:58:20 +0200

    changed convertInput

--- 1st_assignment/Checkers/src/ch/uzh/group38/Game.java
+++ 1st_assignment/Checkers/src/ch/uzh/group38/Game.java
@@ -47,15 +47,14 @@ public class Game {
 
     /*
     converts String input into four variables {x1, y1, x2, y2}
-    [a3]x[b4] for example will become: x1=5, y1=0 and x2=4, y2=1
-    since a3 is board[5][0] and b4 is board [4][1]
+    [a3]x[b4] for example will become: x1=0, y1=2 and x2=1, y2=3
     */
     private static int[] convertInput(String input){
-        int x1 = 8 - Character.getNumericValue(input.charAt(2));
-        int y1 = input.charAt(1) - 'a';
+        int x1 = Character.getNumericValue(input.charAt(2)) - 'a';
+        int y1 = input.charAt(1) - 1;
 
-        int x2 = 8 - Character.getNumericValue(input.charAt(7));
-        int y2 = input.charAt(6) - 'a';
+        int x2 = Character.getNumericValue(input.charAt(7)) - 'a';
+        int y2 = input.charAt(6) - 1;
 
         int[] coordinates = {x1, y1, x2, y2};
         return coordinates;
