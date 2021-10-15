6da15bb779bd1da00a0fdddf60a1896b789255ff
Lany Weizenblut <lanydennise.weizenblutoseguera@uzh.ch>
2021-10-15 14:25:24 +0200

    checkWinner logic

--- 1st_assignment/Checkers/src/ch/uzh/group38/RuleEvaluator.java
+++ 1st_assignment/Checkers/src/ch/uzh/group38/RuleEvaluator.java
@@ -107,7 +107,9 @@ public class RuleEvaluator {
 
     /*
     checks if a player has won the game
+    return 0 if no one has won, 1 if User1 won and 2 if User2 won
     */
-    public static void checkWinner(){  
+    public static int checkWinner(){
+        return 0;
     }
 }
