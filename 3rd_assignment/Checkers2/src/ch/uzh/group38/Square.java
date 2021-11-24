060f2ef16d53d6dcb15c5bd31d34d833f7e4d4e0
UpstairForest <nikita.batalov@uzh.ch>
2021-11-24 08:06:28 +0100

    moved the files to the appropriate location

---
+++ 3rd_assignment/Checkers2/src/ch/uzh/group38/Square.java
@@ -0,0 +1,80 @@
+package ch.uzh.group38;
+
+import javax.swing.*;
+import java.awt.*;
+
+abstract class Square extends JButton {
+    public abstract void setVoidAction();
+    public abstract void setEmptyAction();
+    public abstract void setInactiveAction();
+    public abstract void setActiveAction();
+    public abstract void applyIcon(Icon passedIcon);
+    public abstract void activate();
+    public abstract void deactivate();
+}
+
+class EmptySquare extends Square {
+    GUI.ButtonPressed buttonPressed;
+
+    public EmptySquare(GUI.ButtonPressed passedAction) {
+        this.buttonPressed = passedAction;
+        this.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+        this.addActionListener(passedAction);
+        this.setBackground(Color.white);
+        this.setOpaque(true);
+    }
+
+    public void setVoidAction() {
+        buttonPressed.setState(buttonPressed.getVoidState());
+    }
+    public void setEmptyAction() {
+        buttonPressed.setState(buttonPressed.getEmptyState());
+    }
+    public void setInactiveAction() {
+        buttonPressed.setState(buttonPressed.getInactiveState());
+    }
+    public void setActiveAction() {
+        buttonPressed.setState(buttonPressed.getActiveState());
+    }
+
+    public void applyIcon(Icon passedIcon) {}
+    public void activate() {}
+    public void deactivate() {}
+}
+
+class BlackSquare extends Square {
+    GUI.ButtonPressed buttonPressed;
+
+    public BlackSquare(GUI.ButtonPressed passedAction) {
+        this.buttonPressed = passedAction;
+        this.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+        this.addActionListener(passedAction);
+        this.setBackground(Color.black);
+        this.setOpaque(true);
+    }
+
+    public void setVoidAction() {
+        buttonPressed.setState(buttonPressed.getVoidState());
+    }
+    public void setEmptyAction() {
+        buttonPressed.setState(buttonPressed.getEmptyState());
+    }
+    public void setInactiveAction() {
+        buttonPressed.setState(buttonPressed.getInactiveState());
+    }
+    public void setActiveAction() {
+        buttonPressed.setState(buttonPressed.getActiveState());
+    }
+
+    public void applyIcon(Icon passedIcon) {
+        this.setIcon(passedIcon);
+    }
+
+    public void activate() {
+        this.setBackground(Color.green);
+    }
+
+    public void deactivate() {
+        this.setBackground(Color.black);
+    }
+}
