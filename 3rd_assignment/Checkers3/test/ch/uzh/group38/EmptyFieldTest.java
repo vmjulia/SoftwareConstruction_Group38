964c27781e01a9f8028713563ce0cb0b2142a847
“Julia” <juliaprozorova2@gmail.com>
2021-11-25 09:59:53 +0100

    Create EmptyFieldTest.java

---
+++ 3rd_assignment/Checkers3/test/ch/uzh/group38/EmptyFieldTest.java
@@ -0,0 +1,80 @@
+package ch.uzh.group38;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmptyFieldTest {
+    Field f;
+    @Before
+    public void createField(){
+        f = new EmptyField(1, 2 );
+    }
+
+
+
+    @Test
+    public void testCoordinates(){
+        assertEquals(1, f.getX());
+        assertEquals(2, f.getY());
+         f.updatePosition(2, 1);
+        assertEquals(2, f.getX());
+        assertEquals(1, f.getY());
+    }
+    @Test
+    public void testIsSomething(){
+        assertFalse(f.isKing());
+        assertFalse(f.isWhite());
+        assertFalse(f.isRed());
+        assertTrue(f.isEmpty());
+        f.convertToKing();
+        assertFalse(f.isKing());
+
+    }
+
+    @Test
+    public void testMoveStorage(){
+        Move m;
+
+       Board b  = new Board();
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                if(b.getField(i, j).isEmpty()){
+                    f.update();
+                    assertFalse(f.isAnyMovePossible());
+                    m = new Move (i, j, i+1, j+1);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i-1, j-1);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i+1, j-1);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i-1, j+1);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i+2, j+2);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i-2, j-2);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i+2, j-2);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+                    m = new Move (i, j, i-2, j+2);
+                    assertFalse(f.isMoveStored(m));
+                    assertFalse(f.isJumpMoveStored(m));
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
