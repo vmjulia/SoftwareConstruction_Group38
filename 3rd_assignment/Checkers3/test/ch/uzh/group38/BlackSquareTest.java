9622ebfddeec319336a9b73e341348fe37654d38
UpstairForest <nikita.batalov@uzh.ch>
2021-11-29 02:58:59 +0100

    in between fixing test

--- 3rd_assignment/Checkers3/test/ch/uzh/group38/BlackSquareTest.java
+++ 3rd_assignment/Checkers3/test/ch/uzh/group38/BlackSquareTest.java
@@ -1,6 +1,7 @@
 package ch.uzh.group38;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Constructor;
@@ -9,37 +10,47 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 
 public class BlackSquareTest extends TestCase {
+    GUI gui;
+    Square[][] playBoardSquares;
+
+    @Before
+    public void setUp() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Constructor<GUI> constructor = GUI.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        GUI gui = constructor.newInstance();
+
+        Field pBS = gui.getClass().getDeclaredField("playBoardSquares");
+        pBS.setAccessible(true);
+        this.playBoardSquares = (Square[][]) pBS.get(gui);
+    }
 
     @Test
-    public void setActionTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+    public void testSetAction() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
 
-    Constructor<GUI.ButtonPressed> constructor = GUI.ButtonPressed.class.getDeclaredConstructor();
-    assertTrue(Modifier.isPrivate(constructor.getModifiers()));
-    constructor.setAccessible(true);
-    GUI.ButtonPressed buttonPressed = constructor.newInstance();
+        GUI.ButtonPressed buttonPressed = constructor.newInstance();
 
-    GUI.State voidState = buttonPressed.getVoidState();
-    GUI.State emptyState = buttonPressed.getEmptyState();
-    GUI.State inactiveState = buttonPressed.getInactiveState();
-    GUI.State activeState = buttonPressed.getActiveState();
+        GUI.State voidState = buttonPressed.getVoidState();
+        GUI.State emptyState = buttonPressed.getEmptyState();
+        GUI.State inactiveState = buttonPressed.getInactiveState();
+        GUI.State activeState = buttonPressed.getActiveState();
 
-    Field cS = buttonPressed.getClass().getDeclaredField("currentState");
-    cS.setAccessible(true);
-    GUI.State currentState = (GUI.State) cS.get(buttonPressed);
+        Field cS = buttonPressed.getClass().getDeclaredField("currentState");
+        cS.setAccessible(true);
+        GUI.State currentState = (GUI.State) cS.get(buttonPressed);
 
-    BlackSquare bs =  new BlackSquare(buttonPressed);
+        BlackSquare bs =  new BlackSquare(buttonPressed);
 
-    bs.setVoidAction();
-    assertEquals(voidState, currentState);
+        bs.setVoidAction();
+        assertEquals(voidState, currentState);
 
-    bs.setEmptyAction();
-    assertEquals(emptyState, currentState);
+        bs.setEmptyAction();
+        assertEquals(emptyState, currentState);
 
-    bs.setInactiveAction();
-    assertEquals(inactiveState, currentState);
+        bs.setInactiveAction();
+        assertEquals(inactiveState, currentState);
 
-    bs.setActiveAction();
-    assertEquals(activeState, currentState);
+        bs.setActiveAction();
+        assertEquals(activeState, currentState);
 
 
     }
