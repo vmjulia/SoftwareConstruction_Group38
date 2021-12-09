1c483a3a6e489276a7ef33e03522a8cc73922e50
grueezi <mark.rueetschi@uzh.ch>
2021-12-09 21:18:08 +0100

    removed unused import

--- 4th_assignment/Blackjack/src/ch/uzh/group38/CustomScanner.java
+++ 4th_assignment/Blackjack/src/ch/uzh/group38/CustomScanner.java
@@ -1,7 +1,6 @@
 package ch.uzh.group38;
 
 import java.util.Scanner;
-import java.util.stream.StreamSupport;
 
 public class CustomScanner{
     private static Scanner instance = new Scanner(System.in);
