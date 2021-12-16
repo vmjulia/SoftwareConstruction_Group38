3be23d6c098a0ce2e74d7e4012f2958f8051b1bf
UpstairForest <nikita.batalov@uzh.ch>
2021-12-16 11:31:28 +0100

    slowly populating the methods

--- 4th_assignment/Blackjack/src/ch/uzh/group38/Game.java
+++ 4th_assignment/Blackjack/src/ch/uzh/group38/Game.java
@@ -25,6 +25,6 @@ public class Game {
     }
 
     public static void main(String[] args) {
-
+        new Game();
     }
 }
--- 4th_assignment/Blackjack/src/ch/uzh/group38/Player.java
+++ 4th_assignment/Blackjack/src/ch/uzh/group38/Player.java
@@ -1,5 +1,7 @@
 package ch.uzh.group38;
 
+import java.util.Scanner;
+
 
 interface Observer {
     void update(Iterator iterator);
@@ -11,24 +13,39 @@ interface Aggregate {
 
 public class Player implements Observer, Aggregate{
 
+    private int cash = 100;
     private int bet;
     private int score;
     private Card[] cards;
-
-    // will be updated from game
-    private Card[] dealersCards;
-    private int dealersScore;
 
     public Player() {
 
     }
 
     public int makeBet() {
+        System.out.println("Your current cash: " + this.cash + "\nHow much would you like to bet? ");
+        int bet =  this.readInput();
+        if (bet > this.cash || bet <= 0){
+            makeBet();
+        }
+        this.bet = bet;
         return this.bet;
     }
 
     public void takeTurn() {
 
+    }
+
+    private int readInput(){
+        while (true) {
+            String input = new Scanner(System.in).nextLine();
+            try {
+                int i = Integer.parseInt(input);
+                return i;
+            } catch (Exception NumberFormatException) {
+                System.out.println("Invalid input! Please give an input of type integer");
+            }
+        }
     }
 
     public int countScore() {
@@ -37,7 +54,14 @@ public class Player implements Observer, Aggregate{
 
     @Override
     public void update(Iterator iterator) {
+        // will be updated from Dealer
+        Card[] dealersCards;
+        int dealersScore;
 
+        // cash and score are updated,
+        // exceptions can be sent to dealer and then to game in case player runs out of money
+        this.score = score;
+        this.cash = cash;
     }
 
     @Override
