78397d1a268d0db044884eb5fb1ef08680598c0a
“Julia” <juliaprozorova2@gmail.com>
2021-12-20 18:28:43 +0100

    added comments to Strategy

--- 4th_assignment/Blackjack/src/ch/uzh/group38/Strategy.java
+++ 4th_assignment/Blackjack/src/ch/uzh/group38/Strategy.java
@@ -1,16 +1,16 @@
 package ch.uzh.group38;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
 import java.io.IOException;
 import java.util.Scanner;
-import edu.cmu.sphinx.api.Configuration;
-import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.LiveSpeechRecognizer;
-import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public interface Strategy {
-    public boolean hit(int score) throws IOException, InterruptedException;
+     boolean hit(int score) throws IOException, InterruptedException;
 }
 
 class PlayerStrategy implements Strategy{
@@ -43,13 +43,15 @@ class PlayerVoiceStrategy implements Strategy{
     private final LiveSpeechRecognizer recognizer;
 
     public PlayerVoiceStrategy() throws IOException {
+        // set up configuration once initialized
         configuration = new Configuration();
         handleConfiguration();
         recognizer = new LiveSpeechRecognizer(configuration);
     }
 
     @Override
-    public boolean hit(int score) throws IOException, InterruptedException {
+    public boolean hit(int score) throws  InterruptedException {
+        // get the string via voice input and interpret yes as hit
         String input = VoiceInput();
         switch (input) {
             case "no": break;
@@ -59,10 +61,12 @@ class PlayerVoiceStrategy implements Strategy{
     }
 
     private void handleConfiguration() {
+        // standard paths set up
 
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+        // grammar includes 2 words only: yes/no, optimizes processing time
         configuration.setGrammarPath("file:4th_assignment/Blackjack/src");
         configuration.setGrammarName("words");
         configuration.setUseGrammar(true);
@@ -71,6 +75,7 @@ class PlayerVoiceStrategy implements Strategy{
 
     private String VoiceInput() throws InterruptedException {
 
+        // do not output ino messages of Sphinx4 to console
         Logger cmRootLogger = Logger.getLogger("default.config");
         cmRootLogger.setLevel(java.util.logging.Level.OFF);
         String conFile = System.getProperty("java.util.logging.config.file");
@@ -78,15 +83,21 @@ class PlayerVoiceStrategy implements Strategy{
             System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput");
         }
 
+
+
         System.out.println("Do you want to hit [Yes/No] ? ");
+        // hold on to leave time for thinking
         TimeUnit.SECONDS.sleep(3);
         recognizer.startRecognition(true);
-        System.out.println("speak now");
+        // start talking when message appears
+        System.out.println("speak now...");
         SpeechResult result;
+        // repeat until a clear is received
         while (true)
         {
             result = recognizer.getResult();
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
+            // print hypothesis and break the loop once clear input is received
             if (result.getHypothesis().equals( "yes")|| result.getHypothesis().equals( "no")){
                 break;
 
@@ -94,6 +105,8 @@ class PlayerVoiceStrategy implements Strategy{
             System.out.println("speak now");
     }
         recognizer.stopRecognition();
+
+        // return the recognized string
         return  (result.getHypothesis());
     }
 }
@@ -102,6 +115,7 @@ class PlayerVoiceStrategy implements Strategy{
 
 class DealerStrategy implements Strategy{
     @Override
+    // hit if score is lower than 17
     public boolean hit(int score){
         if (score < 17) {
             System.out.println("Dealer hits\n");
