e908d7e95e1fe7b83129f1b6228f7827d157364d
UpstairForest <nikita.batalov@uzh.ch>
2021-12-20 04:27:30 +0100

    Test Deck.shuffle()

--- 4th_assignment/Blackjack/test/ch/uzh/group38/DeckTest.java
+++ 4th_assignment/Blackjack/test/ch/uzh/group38/DeckTest.java
@@ -4,21 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Stack;
+
 import static org.junit.Assert.*;
 
 
 public class DeckTest {
-    Deck d;
+    Deck deck;
 
     @Before
     public void createDeck() {
-        d = Deck.getInstance();
+        deck = Deck.getInstance();
     }
 
     @After
     public void resetDeck() {
         // since Deck is a Singleton, not resetting it might affect other tests
-        d.putDiscardBack();
+        deck.putDiscardBack();
     }
 
 
@@ -26,16 +30,45 @@ public class DeckTest {
     public void testTwoInstances() {
         Deck dd;
         dd = Deck.getInstance();
-        assertEquals(d, dd);
+        assertEquals(deck, dd);
     }
 
     @Test
     public void testDrawCard() {
         for (int i = 0; i <= 51; i++) {
-            Card c = d.draw();
+            Card c = deck.draw();
             assertNotNull(c);
         }
-        Card c = d.draw();
+        Card c = deck.draw();
         assertNull(c);
+    }
+
+    // should have a 1 in 10^67 chance to fail
+    @Test
+    public void testShuffle() throws NoSuchFieldException, IllegalAccessException {
+        Field cardsField = deck.getClass().getDeclaredField("cards");
+        cardsField.setAccessible(true);
+        Stack<Card> cardsAfter = (Stack) cardsField.get(deck);
+        Stack<Card> cardsBefore = new Stack<>();
+        for (int i = 0; i < 52; i++) {
+            cardsBefore.add(cardsAfter.get(i));
+        }
+
+        // check that contents are identical
+        for (int i = 0; i < 52; i++) {
+            if (!cardsAfter.get(i).equals(cardsBefore.get(i))) {
+                fail();
+            }
+        }
+
+        deck.shuffle();
+
+        // check that contents are no longer identical
+        for (int i = 0; i < 52; i++) {
+            if (!cardsAfter.get(i).equals(cardsBefore.get(i))) {
+                return;
+            }
+        }
+        fail();
     }
 }
